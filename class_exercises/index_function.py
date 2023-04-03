76e2954d7d23e7ce578e2a6df43f9a6a0e12cca8
9KICKS <officialjoshua9@gmail.com>
2023-04-03 20:23:36 +0100

    "added index_function.py"

---
+++ class_exercises/index_function.py
@@ -0,0 +1,18 @@
+def get_index(x, y, z):
+    total = 0
+    i = 0
+
+    while total < y:
+        i += 1
+        total += x
+
+    if total == y and i > 0 and (i * x) - (x - z) <= y:
+        return i
+    else:
+        return - 1
+
+
+x = 3
+y = 12
+z = 1
+print(get_index(x, y, z))
