c216e82d16b44d5408d95f9b65d49d20db0f0f40
9KICKS <officialjoshua9@gmail.com>
2023-04-14 13:59:41 +0100

    "added mississippi.py"

---
+++ semicolon_assessment/level1/mississippi.py
@@ -0,0 +1,6 @@
+word = "Mississippi"
+count_for_letter_s = word.count('s')
+count_for_letter_i = word.count('i')
+
+print("Number of occurrences of 's': ", count_for_letter_s)
+print("Number of occurrences of 'i': ", count_for_letter_i)
